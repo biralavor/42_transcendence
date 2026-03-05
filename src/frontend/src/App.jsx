934782fdeeeebc64df8af5d3dfddc870f8303923
Bruno <bruno.lameirinha@gmail.com>
2026-03-05 05:39:44 -0300

    feat(front): setup react-router-dom navigation
    Create pages, configure routes, and extract Navbar component. Refs #40

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import About from './pages/About'
+import Play from './pages/Play'
+import Leaderboard from './pages/Leaderboard'
+
 
 export default function App() {
   return (
@@ -8,6 +12,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
       </Routes>
     </BrowserRouter>
   )
