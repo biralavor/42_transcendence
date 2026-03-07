3c9ee5cd02332d48eb21b429846023898ff6be59
Bruno Nespoli Thorstensen <bnespoli@c3r13p2.42sp.org.br>
2026-03-07 17:28:21 -0300

    wip: #33 process requests for authentication

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from models.credentials import Credentials
+from models.login import Login
+from models.token import Token
 
 app = FastAPI(title="Transcendence API")
 
@@ -11,3 +14,19 @@ def health():
 @app.get("/")
 def root():
     return {"message": "Transcendence API stub"}
+
+@app.post("/test")
+def test():
+    return {"message": "Post test"}
+
+@app.post("/auth/register", status_code=status.HTTP_201_CREATED)
+def register(credentials: Credentials):
+    return credentials
+
+@app.post("/auth/login", status_code=status.HTTP_200_OK)
+def login(login: Login):
+    external_data = {
+    'access_token': "token1"
+    }
+    token = Token(**external_data)
+    return token
